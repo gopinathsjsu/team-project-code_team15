e46aeeb5ce54492fb3017de689a54f71e75d41ad
AbhiramAtluri <saiabhiram.atluri@sjsu.edu>
2022-04-12 21:17:59 -0700

    Adding Navbar

--- HotelBooking/hotelbookingfrontend/src/App.js
+++ HotelBooking/hotelbookingfrontend/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
+
     <div className="App">
+      <NavBar></NavBar>
   
     </div>
+
   );
 }
 
