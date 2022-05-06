2711c42472326f968e19ff799b1719c83455f738
Harsha Anirudh Garre <harsha.anirudh@gmail.com>
2022-05-06 15:16:56 -0700

    Search

--- HotelBooking/hotelbookingBackend/Validations/searchValidations.js
+++ HotelBooking/hotelbookingBackend/Validations/searchValidations.js
@@ -1,3 +1,4 @@
+var pool = require('./../Database/db')
 class searchHandler{
     setNextObj(nextObj){}
     validate(req,res){
@@ -13,8 +14,11 @@ class checkIfDatesAreValid extends searchHandler{
         this.nextObj=nextObj;
     }
     validate(req,res){
-        let isValid=new Date(req.body.checkin) < new Date(req.body.checkout)
-        console.log("check in"+new Date(req.body.checkin))
+        let checkIn=new Date(req.body.checkin);
+        let checkOut=new Date(req.body.checkout)
+        const diffDays = Math.ceil((checkOut-checkIn) / (1000 * 60 * 60 * 24)); 
+        let isValid= (checkIn<checkOut && (diffDays<=7))
+        console.log(diffDays)
         if(isValid){
             this.nextObj.validate(req,res);
         }
@@ -25,6 +29,36 @@ class checkIfDatesAreValid extends searchHandler{
         }
     }
 }
+
+class RoomsAndGuestNoIsValid extends searchHandler{
+    constructor(){
+        super()
+        this.nextObj= new searchHandler();
+        this.isValid=true;
+    }
+    setNextObj(nextObj){
+        this.nextObj=nextObj;
+    }
+    async validate(req,res){
+        let {roomType,noOfRooms,noOfGuests}=req.body;
+        if((roomType==='single') && (noOfGuests>noOfRooms*2)){
+            this.isValid=false;
+        }else if((roomType==='double') && (noOfGuests>noOfRooms*4)){
+            this.isValid=false;
+        }else if((roomType==='suite')&& (noOfGuests>noOfRooms*2)){
+            this.isValid=false;
+        }
+
+        if(this.isValid){
+            this.nextObj.validate(req,res);
+        }else{
+            res.status(401).send({
+                message:"invalid no of guests for rooms"
+            })
+        }
+    }
+}
+
  class processSearch extends searchHandler{
     constructor(){
         super()
@@ -33,14 +67,17 @@ class checkIfDatesAreValid extends searchHandler{
     setNextObj(nextObj){
 
     }
-    validate(req,res){
+    async validate(req,res){
         console.log("processing search");
-        res.status(200).send({
-            message:"done"
-        })
+        let {location,checkin,checkout,roomType,noOfRooms,noOfGuests}= req.body
+        let searchForRooms=`select t1.hotelId,t1.city,t1.hotelName,t2.roomId,t2.roomNo,t2.roomCapacity,t2.basePrice, t2.roomType from Hotels as t1 left join (select roomID,hotelId,roomNo,basePrice,roomType,roomCapacity from Rooms where roomID not in 
+            (select roomId from Reservations where STR_TO_DATE(?,'%m/%d/%Y') < STR_TO_DATE(checkOutDate,'%m/%d/%Y') and STR_TO_DATE(?,'%m/%d/%Y')> STR_TO_DATE(checkInDate,'%m/%d/%Y'))) as t2 on t1.hotelId= t2.hotelId where t1.city like ? and t2.roomType=? ;
+            `
+        let result=await pool.query(searchForRooms,[checkin,checkout,'%'+location+'%',roomType]);
+        res.status(200).send(result[0])
     }
 }
-module.exports={processSearch,checkIfDatesAreValid}
+module.exports={processSearch,checkIfDatesAreValid,RoomsAndGuestNoIsValid}
 
 
 
--- HotelBooking/hotelbookingBackend/app.js
+++ HotelBooking/hotelbookingBackend/app.js
@@ -13,26 +13,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
   res.send('Welcome to Hotel Reservation Backend API');
 });
-const mysql = require("mysql2")
+// const mysql = require("mysql2")
 
-const conn = mysql.createConnection(
-  {
-    host: "Booking202.cgufr2o7hllp.us-east-2.rds.amazonaws.com",
-    database: "Booking",
-    user: "admin",
-    password: "April2020",
-    port: 3306
+// const conn = mysql.createConnection(
+//   {
+//     host: "Booking202.cgufr2o7hllp.us-east-2.rds.amazonaws.com",
+//     database: "Booking",
+//     user: "admin",
+//     password: "April2020",
+//     port: 3306
 
-  }
-)
+//   }
+// )
 
-conn.connect((err) => {
-  if (err) {
-    return console.log(err)
-  }
-  console.log("Connected")
+// conn.connect((err) => {
+//   if (err) {
+//     return console.log(err)
+//   }
+//   console.log("Connected")
 
-})
+// })
 
 app.listen(3001, function () {
   console.log('Server Listening on port 3000');
