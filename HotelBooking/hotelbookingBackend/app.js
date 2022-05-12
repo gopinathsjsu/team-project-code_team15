9477ffcdc9e846d5f778786cdca377621295e692
Anuhya Gankidi <gankidianuhya@gmail.com>
2022-05-11 18:49:59 -0700

    Change server port

--- HotelBooking/hotelbookingBackend/app.js
+++ HotelBooking/hotelbookingBackend/app.js
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
 
 // })
 var port=normalizePort(process.env.PORT || '3001');
-app.listen(process.env.PORT, function () {
+app.listen(port, function () {
   console.log('Server Listening on port '+port);
 });
 app.use("/customer",customerRouter)
