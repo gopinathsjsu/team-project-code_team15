194d3836df48b1cd4b4c46616dd15dea97d72bfc
Anuhya Gankidi <gankidianuhya@gmail.com>
2022-05-12 00:02:19 -0700

    Adding My Bookings to Nav Bar

--- HotelBooking/hotelbookingfrontend/src/NavBar.js
+++ HotelBooking/hotelbookingfrontend/src/NavBar.js
@@ -1,13 +1,15 @@
 
 import React from 'react'
 import { Button, Navbar,Container,Nav,NavDropdown ,Form,FormControl } from 'react-bootstrap'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 export default function NavBar() {
-
-  
+  const [myBookingDisplay,setmyBookingDisplay] = useState(true)
+   
     return (
-<Navbar bg="light" expand="lg">
+<Navbar  expand="lg">
   <Container fluid>
-    <Navbar.Brand href="/">Booking.com</Navbar.Brand>
+    <Navbar.Brand href="/">Avalon.com</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
@@ -15,31 +17,11 @@ export default function NavBar() {
         style={{ maxHeight: '110px' }}
         navbarScroll
       >
-        {/* <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link> */}
-        {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown> */}
-        {/* <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link> */}
+
       </Nav>
-      {/* <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form> */}
-      <a className='d-flex' style={{"cursor":"pointer"}}>Sign In</a>
-      {/* <a className='d-flex' style={{"cursor":"pointer"}}>Bookings</a> */}
+
+      <Link className='d-flex' to={{pathName:"/Bookings"}} style={{"cursor":"pointer",textDecoration:"none"}} >{myBookingDisplay==true?"MyBookings":""}  </Link>
+    
     </Navbar.Collapse>
   </Container>
 </Navbar>
